5f4d8fcf1e189558f65f64a96793d7c1019c2def
Sadikul Mobasshir <114331112+MOBASSHIR07@users.noreply.github.com>
2026-05-21 00:45:04 +0600

    slice note

--- Slice/main.go
+++ Slice/main.go
@@ -6,7 +6,14 @@ func main(){
 	//slice:=order[2:] // 2 index to last
 	//slice:=order[:] //  all element 
 	// slice ager array k change kore dey
+         //slice শুরু হওয়ার index থেকে original array এর শেষ পর্যন্ত কয়টা element available।
 	// slice len , capacity , pointer thake
+	//কতগুলো element currently আছে।
+	//Go তে slice change করলে original array ও change হয় — কারণ slice copy না, underlying array কে reference করে।
+	//Slice নিজে dynamic মনে হলেও আসলে এটা fixed-size underlying array এর উপর reference হিসেবে কাজ করে, আর capacity শেষ হলে Go নতুন বড় array বানিয়ে data copy করে বলে slice dynamically grow করতে পারে।
+	//Slice এ নতুন element add করতে সাধারণত append() ব্যবহার করা হয়
+	fmt.Println(cap(slice))
+	fmt.Println(len(slice))
 
 	fmt.Println(slice)
 }
